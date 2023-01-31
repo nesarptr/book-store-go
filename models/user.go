192adc416b5972aecb3b7a65a8f533feea33f1de
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-01-31 16:39:44 +0600

    before create hook created for user model

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 )
 
@@ -11,4 +13,13 @@ type User struct {
 	Password string  `json:"password" validate:"required,min=6"`
 	Books    []Book  `json:"books" validate:"dive"`
 	Orders   []Order `json:"orders" validate:"dive"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) error {
+	foundUser := User{}
+	tx.Where("email = ?", user.Email).First(&foundUser)
+	if foundUser.Name != "" {
+		return errors.New("User Already Exist with this email")
+	}
+	return nil
 }
