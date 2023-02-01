ad4f351acafda9729d8bc72b96a0c5d0e91dad1b
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-02-02 00:20:17 +0600

    order model modified

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/stripe/stripe-go/v74 v74.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- models/order.go
+++ models/order.go
@@ -6,6 +6,8 @@ type Order struct {
 	gorm.Model
 	TotalPrice float64     `json:"totalPrice" validate:"required,gt=0"`
 	Books      []OrderItem `json:"books" validate:"dive"`
+	IsPaid     bool        `json:"isPaid" validate:"required"`
+	PaymentID  string      `json:"paymentId" validate:"-"`
 	UserID     uint        `json:"-" validate:"required"`
 	Owner      User        `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:ID" validate:"-"`
 }
