37088208bbdf7834fe2445fb90097c885f9fed0b
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-02-01 20:54:07 +0600

    cart item model modified

--- models/cart-item.go
+++ models/cart-item.go
@@ -6,9 +6,10 @@ import (
 
 type CartItem struct {
 	gorm.Model
-	BookID   uint `json:"bookId" validate:"required"`
-	CartID   uint `json:"cartId" validate:"required"`
-	Book     Book `json:"book" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:BookID;references:ID" validate:"-"`
-	Cart     Cart `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:CartID;references:ID" validate:"-"`
-	Quantity int  `json:"quantity" validate:"required,gt=0"`
+	BookID   uint    `json:"bookId" validate:"required"`
+	CartID   uint    `json:"cartId" validate:"required"`
+	Book     Book    `json:"book" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:BookID;references:ID" validate:"-"`
+	Cart     Cart    `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:CartID;references:ID" validate:"-"`
+	Quantity int     `json:"quantity" validate:"required,gt=0"`
+	Price    float64 `json:"-" validate:"required,gt=0"`
 }
