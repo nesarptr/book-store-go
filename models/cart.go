a02846f96cf650d3ef5bd9dbf969425c79362321
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-02-01 19:47:22 +0600

    cart model modified

--- models/cart.go
+++ models/cart.go
@@ -6,8 +6,8 @@ import (
 
 type Cart struct {
 	gorm.Model
-	TotalPrice float64    `json:"totalPrice" validate:"required,gt=0"`
-	Books      []CartItem `json:"books" validate:"dive"`
+	TotalPrice float64    `json:"totalPrice" validate:"required,gte=0"`
+	Books      []CartItem `json:"books" validate:"-"`
 	UserID     uint       `json:"-" validate:"required"`
 	Owner      User       `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:ID" validate:"-"`
 }
